cea3e6988723135189ccd761326e9804928faa3b
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-08 11:02:23 +0300

    enhance file handling in ChatController with encryption support and improved error handling

--- Controller/ChatController.java
+++ Controller/ChatController.java
@@ -1,19 +1,31 @@
 package Controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 import Models.Message;
+import Networking.FileSender;
+import Networking.TransferListener;
 import Service.ChatService;
 import Service.FileService;
 import Service.SessionManager;
+import Util.EncryptionUtil;
 import Models.FileMessage;
 import javafx.geometry.Pos;
 
@@ -26,40 +38,54 @@ public class ChatController {
     @FXML private Button attachFileButton;
 
     private ChatService chatservice = new ChatService();
+    private static final String serverIP = "10.0.0.1";
+    private static final int port = 360;
 
     @FXML
     private void initialize(){
         loadActiveChats();
         sendButton.setOnAction(_ -> sendMessage());
-        attachFileButton.setOnAction(_ -> sendFile());
+        attachFileButton.setOnAction(_ -> sendFile(null));
     }
 
     //handle file download when clicked
     public void onFileClicked(FileMessage fileMessage) {
         //get the file from the storage dir
-        File receivedFile = FileService.getFile(fileMessage.getFileName());
-        if (receivedFile != null) {
-            System.err.println("Downloading:" + receivedFile.getAbsolutePath());
+        File encryptedFile = FileService.getFile(fileMessage.getFileName() + ".enc");
+        if (encryptedFile != null) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle(" Decrypted File");
+            fileChooser.setInitialFileName(fileMessage.getFileName());
+            fileChooser.getExtensionFilters().add(new ExtensionFilter("All Files", "*.*"));
 
-            //open the file in the default application
-            openFile(receivedFile);
-        }else{
-            System.err.println("File not found");
-        }
-    }
-
-    //helper method to open a file(windows, macOS , LInux support)
-    private void openFile(File file) {
-        try{
-            if(file.exists()){
-                java.awt.Desktop.getDesktop().open(file);
-            }else{
-                System.err.println("File does not exist");
+            File saveLocation =fileChooser.showOpenDialog(chatListView.getScene().getWindow());
+            if(saveLocation != null){
+                try{
+                    //decrypt using stream
+                    EncryptionUtil.decryptFile(encryptedFile, saveLocation);
+                    System.out.println("File decrypted and saved to:" + saveLocation.getAbsolutePath());
+                }catch(IOException e){
+                    e.printStackTrace();
+                    showError("Failed to save file");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else{
+                showError("File not found!");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
     }
+
+    //error handling
+    private void showError(String Error){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(Error);
+        alert.showAndWait();
+    }
+
 
     private void loadActiveChats(){
         activeChatList.getItems().clear();
@@ -88,9 +114,24 @@ public class ChatController {
     }
 
     private void addMessageToChat(Message message) {
-        Text msgText = new Text(message.getSender() + ": " + message.getContent());
-        msgText.setFill(message.getSender().equals("Me") ? Color.GREEN : Color.BLACK);
-        TextFlow textFlow = new TextFlow(msgText);
+        TextFlow textFlow;
+        if(message instanceof FileMessage){
+            FileMessage fileMessage = (FileMessage) message;
+
+            Text fileText = new Text("[File:" + fileMessage.getFileName() + "](Click to Download)");
+            fileText.setFill(Color.BLUE);
+            fileText.setUnderline(true);
+
+            //click event to download file
+            fileText.setOnMouseClicked(_ -> onFileClicked(fileMessage));
+
+            textFlow = new TextFlow(fileText);
+        }else{
+            Text msgText = new Text(message.getSender() + ":" + message.getContent());
+            msgText.setFill(message.getSender().equals("Me") ? Color.GREEN : Color.BLACK);
+            textFlow = new TextFlow(msgText);
+        }
+
         textFlow.setStyle("-fx-background-color: #e3e3e3; -fx-padding: 8px; -fx-background-radius: 10;");
         
         HBox messageBox = new HBox(textFlow);
@@ -99,33 +140,36 @@ public class ChatController {
         chatListView.getItems().add(messageBox);
     }
 
-    private void sendFile() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select File to Send");
-        File file = fileChooser.showOpenDialog(null);
+    private void sendFile(File file) {
+        try{
+            Socket socket = new Socket(serverIP, port);
+            OutputStream outputStream = socket.getOutputStream();
 
-        if (file != null) {
-            String sender = SessionManager.getLoggedInUser();
-            String receiver = getSelectedReceiver();
+            FileSender.sendFile(file, outputStream,new TransferListener() {
+                ProgressBar progressBar = new ProgressBar();
+                @Override
+                public void onProgress(int percentage){
+                    Platform.runLater(() -> progressBar.setProgress(percentage / 100.0));
+                }
 
-            if(sender == null || receiver == null){
-                System.err.println("Users not logged in");
-                return;
-            }
+                @Override
+                public void onComplete(String message){
+                    Platform.runLater(() -> showInfo(message));
+                }
 
-            FileMessage filemessage = FileService.shareFile(file, sender);
-
-            if(filemessage != null){
-                chatservice.sendMessage(filemessage);
-                addMessageToChat(filemessage);
-            } else {
-                System.err.println("Failed to share file");
-            }
+                @Override
+                public void onError(String errorMessage){
+                    Platform.runLater(() -> showError(errorMessage));
+                }
+            });
+            socket.close();
+        } catch(IOException e){
+            showError("Failed to send file:" + e.getMessage());
         }
+        
+    }
+    private void showInfo( String message){
+
     }
 
-    private String getSelectedReceiver() {
-        Text selectedChat = activeChatList.getSelectionModel().getSelectedItem();
-        return selectedChat != null ? selectedChat.getText() : null;
-    }
 }
