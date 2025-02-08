9fd937abe2c43d4c90678bcb1050c5a278786c4d
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-08 15:52:21 +0300

    add DatabaseUtil import to App class

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import Util.DatabaseUtil;
 
 public class App extends Application {
 
