504eafd9a1a92a5374fdf53e48b512f8d29b1d00
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-08 16:25:35 +0300

    add UserDAO class with loginUser method for user authentication

---
+++ Database/UserDAO.java
@@ -0,0 +1,14 @@
+package Database;
+
+import Models.User;
+
+public class UserDAO {
+
+    public static User loginUser(String username, String password) {
+        // This method should return a User object if the username and password are correct
+        // Otherwise, it should return null
+        return null;
+        
+    }
+    
+}
