01dae6a9ec0f00abcc3eceaf571315ed464f4291
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-09 13:05:02 +0300

    add user registration method to UserDAO and update loginUser to include email

--- Database/UserDAO.java
+++ Database/UserDAO.java
@@ -1,14 +1,52 @@
 package Database;
 
 import Models.User;
+import java.sql.*;
 
 public class UserDAO {
+    // Register a new user
+    public static boolean registerUser(String username, String password, String email) {
+        String query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
 
-    public static User loginUser(String username, String password) {
-        // This method should return a User object if the username and password are correct
-        // Otherwise, it should return null
-        return null;
-        
+        try (Connection conn = DBHelper.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password); // TODO: Hash the password before storing
+            pstmt.setString(3, email);
+
+            int rowsInserted = pstmt.executeUpdate();
+            return rowsInserted > 0;
+
+        } catch (SQLException e) {
+            System.out.println("Error registering user: " + e.getMessage());
+            return false;
+        }
     }
-    
+
+    // Authenticate user (Login)
+    public static User loginUser(String username, String password) {
+        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try (Connection conn = DBHelper.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password); // TODO: Use hashed password verification
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new User(
+                        rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("Login error: " + e.getMessage());
+        }
+        return null;
+    }
 }
+
