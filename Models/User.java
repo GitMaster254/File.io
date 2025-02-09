e5116fca1e37e7b61a8ff2ede7c219406dbacabf
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-09 14:14:41 +0300

    add salt field to User model for enhanced password security

--- Models/User.java
+++ Models/User.java
@@ -5,12 +5,14 @@ public class User {
     private String username;
     private String password;
     private String email;
+    private String salt;
 
-    public User(int id, String username, String password, String email) {
+    public User(int id, String username, String password, String email, String salt) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
+        this.salt = salt;
     }
 
     public int getId() {
@@ -28,4 +30,9 @@ public class User {
     public String getEmail(){
         return email;
     }
+
+    public String getSalt(){
+        return salt;
+    }
+    
 }
