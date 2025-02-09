e7959246766d1ba9ae3f2f6126aa16810fa1d6e1
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-09 13:04:46 +0300

    add email field to User model and update constructor

--- Models/User.java
+++ Models/User.java
@@ -4,11 +4,13 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String email;
 
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     public int getId() {
@@ -21,5 +23,9 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getEmail(){
+        return email;
     }
 }
