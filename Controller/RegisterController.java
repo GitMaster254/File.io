68eca9edb0facc25901b74596eb4f4bcd8e8ec77
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-09 13:32:01 +0300

    add database imports and encryption service to RegisterController

--- Controller/RegisterController.java
+++ Controller/RegisterController.java
@@ -9,6 +9,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+
+import Service.EncryptionService;
+import Database.DBHelper;
 
 public class RegisterController {
 
