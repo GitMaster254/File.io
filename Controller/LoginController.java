38b562ed579529e4241e60180d90ce64585a7658
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-10 19:48:18 +0300

    add show password checkbox to LoginController for enhanced user experience

--- Controller/LoginController.java
+++ Controller/LoginController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -27,6 +28,9 @@ public class LoginController {
 
     @FXML
     private Hyperlink registerLink;
+
+    @FXML
+    private CheckBox showPasswordCheckBox;
 
     @FXML
     private void initialize() {
