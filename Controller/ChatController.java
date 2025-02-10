a9c8ade12cc7227091627c3bfb6337c076b50d30
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2025-02-10 19:06:15 +0300

    add Label import to ChatController for UI enhancements

--- Controller/ChatController.java
+++ Controller/ChatController.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
